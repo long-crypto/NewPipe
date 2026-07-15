d15e3eaea44f1d2cc4f2e3fb36bcea6596f1aea0
long-crypto <175532236+long-crypto@users.noreply.github.com>
2026-07-15 09:03:33 +0800

    Enable BiliBili service

--- app/src/main/java/org/schabi/newpipe/util/ServiceHelper.kt
+++ app/src/main/java/org/schabi/newpipe/util/ServiceHelper.kt
@@ -22,7 +22,7 @@ import org.schabi.newpipe.ktx.getStringSafe
 
 object ServiceHelper {
     private val DEFAULT_FALLBACK_SERVICE: StreamingService = ServiceList.YouTube
-    private val TEMPORARILY_HIDDEN_SERVICE_IDS = setOf(5, 6)
+    private val TEMPORARILY_HIDDEN_SERVICE_IDS = setOf(6)
 
     @JvmStatic
     fun isServiceVisible(service: StreamingService): Boolean {
